d089730c10fa244f34b9f9da5835064aa1441066
Lukas <lukassanz11@gmail.com>
2022-11-01 21:42:27 +0100

    Eigene Programme

--- Eigene_Programme/Vor_dem_05.10.2022/Addierer/Rechner.py
+++
@@ -1,8 +0,0 @@
-print("Hallo, das ist ein simpler Adierer. Gib einfach 2 Zahlen ein und sie werden addiert.")
-print("""Hinweis: Gleitkommazahlen bitte mit "." und nicht mit "," schreiben, sonst entsteht ein ValueError.""")
-number_1 = float(input("Zahl 1: "))
-number_2 = float(input("Zahl 2: "))
-
-print("Das Ergebnis der Addition ist: ")
-print(number_1 + number_2)
-yes_or_no = input("Falls du noch etwas addieren willst, starte bitte das Programm neu.")
--- Eigene_Programme/Vor_dem_05.10.2022/README.md
+++
@@ -1,7 +0,0 @@
-~ 05.10.2022
-
-Wichtig:
-Das sind die ersten Programme die ich geschrieben habe und daher können sie viele Fehler enthalten und/oder schlecht geschrieben sein!
-
-Anmerkung:
-Das Programm "Bruteforce_Programm" ist nicht fertig geschrieben und daher kann es sein, dass es nicht, oder nur mit Fehlern funktioniert! Und ja, das Programm war mit Abstand das Komplexeste und schwierigste für mich.^^
